862077e31f93b55a172e733998e9334e06f4c6bf
syprogrammer <sonalyadavbca2021@gmail.com>
2022-11-07 20:49:13 +0530

    Added Pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-      config.resolve.fallback.tls = false;
-      config.resolve.fallback.net = false;
-      config.resolve.fallback.child_process = false;
-    }
-
-    return config;
-  },
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+});
+module.exports = withPWA({
+  skipWaiting: true,
+});
