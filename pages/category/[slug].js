e397b3481b10318bcef3fa93fe4117e14a017a69
syprogrammer <sonalyadavbca2021@gmail.com>
2022-11-18 13:23:06 +0530

    redeploy

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,6 +1,6 @@
 import groq from "groq";
 import faqbanner from "../../public/images/faqbanner.png";
-import Image from "next/Image";
+import Image from "next/image";
 import { createClient } from "next-sanity";
 
 
